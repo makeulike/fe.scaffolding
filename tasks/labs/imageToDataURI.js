c4d843f09e6b1567efc77fdd57771a862266fbe0
hagi4u <hagi4u@gmail.com>
2016-08-31 10:55:34 +0900

    초기화 파일 변경 및 imageToDataURI rem 변환 기능 추가

--- tasks/labs/imageToDataURI.js
+++ tasks/labs/imageToDataURI.js
@@ -3,8 +3,16 @@ var gulp = require('gulp-param')(require('gulp'), process.argv)
   concat = require('gulp-concat'),
   imageDataURI = require('gulp-image-data-uri');
 
-
 var config = require('../config');
+
+var imageToDataURIConfig = {
+  customClass: function(className, file){
+        var string = className + '.';
+        var filePath = file.path.toString().split('\\');
+
+        return 'uri-' + filePath[filePath.length - 2] + '.' + className    
+  }
+}
 
 /*
  * ## 160219
@@ -14,7 +22,7 @@ var config = require('../config');
  * @Anchor: Jeong MyoungHakl
  */
 
-gulp.task('imageToDataURI', function(product) {
+gulp.task('imageToDataURI:pc', function(product, isRem) {
 
   /**
    * 각 프로젝트 별 개발 경로를 SRC로 변환하는 작업이 필요.
@@ -25,19 +33,44 @@ gulp.task('imageToDataURI', function(product) {
     config.setBasePath(config.path.base);
   }
 
-  return gulp.src(config.path.src[1] + '/images/uri/**/*')
+  var templateSrc = (isRem) ? './templates/data-uri-rem.css' : './templates/data-uri.css'
+
+  return gulp.src(config.path.src[0] + '/images/uri/**/*')
+    //.pipe()
     .pipe(imageDataURI({
       template: {
-        file: './templates/data-uri.css'
+        file: templateSrc
       },
-      customClass: function(className, file) {
-        var string = className + '.';
-        var filePath = file.path.toString().split('\\');
+      customClass: imageToDataURIConfig.customClass
+    }))
+    .pipe(concat('_data-uri.scss'))
+    .pipe(gulp.dest(config.path.src[0] + '/scss/modules'));
 
-        return 'uri-' + filePath[filePath.length - 2] + '.' + className;
-      }
+});
+
+gulp.task('imageToDataURI:mobile', function(product, isRem) {
+
+  /**
+   * 각 프로젝트 별 개발 경로를 SRC로 변환하는 작업이 필요.
+   * @date 151113
+   */
+  if (product !== null) {
+    config.path.base = 'archive/' + product;
+    config.setBasePath(config.path.base);
+  }
+
+  var templateSrc = (isRem) ? './templates/data-uri-rem.css' : './templates/data-uri.css'
+
+  return gulp.src(config.path.src[1] + '/images/uri/**/*')
+    //.pipe()
+    .pipe(imageDataURI({
+      template: {
+        file: templateSrc
+      },
+      customClass: imageToDataURIConfig.customClass
     }))
     .pipe(concat('_data-uri.scss'))
     .pipe(gulp.dest(config.path.src[1] + '/scss/modules'));
+
 });
 // ------------- Labs finished -------------- //
